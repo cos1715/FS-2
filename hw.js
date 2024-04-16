fc911c72350386097ff8fad3c5c320d9cde48c56
Taras Samoilenko <tsamoilenko97@gmail.com>
2024-04-16 19:41:46 +0300

    hw

--- hw.js
+++ hw.js
@@ -68,24 +68,64 @@
 
 // rhombus
 
-const size = +prompt();
+// const size = +prompt();
 
-const halfRow = Math.floor(size / 2);
-let dotsLength = halfRow;
-let cagesLength = 1;
+// const halfRow = Math.floor(size / 2);
+// let dotsLength = halfRow;
+// let cagesLength = 1;
 
-for (let i = 0; i < halfRow; i++) {
-  const dots = "".padStart(dotsLength, ".");
-  const cage = "".padStart(cagesLength, "#");
-  console.log(dots + cage + dots);
-  cagesLength += 2;
-  dotsLength--;
+// for (let i = 0; i < halfRow; i++) {
+//   const dots = "".padStart(dotsLength, ".");
+//   const cage = "".padStart(cagesLength, "#");
+//   console.log(dots + cage + dots);
+//   cagesLength += 2;
+//   dotsLength--;
+// }
+
+// for (let j = 0; j < halfRow + 1; j++) {
+//   const dots = "".padStart(dotsLength, ".");
+//   const cage = "".padStart(cagesLength, "#");
+//   console.log(dots + cage + dots);
+//   cagesLength -= 2;
+//   dotsLength++;
+// }
+
+const tableContainer = document.getElementById("table");
+const table = document.createElement("table");
+table.style.border = "1px solid black";
+
+for (let i = 0; i <= 10; i++) {
+  const tr = document.createElement("tr");
+
+  for (let j = 0; j <= 10; j++) {
+    const cell = document.createElement("td");
+    cell.style.border = "1px solid black";
+    cell.style.padding = "10px";
+    cell.innerText = i === 0 || j === 0 ? j || i : j * i;
+
+    const highlightCells = (cellColor = "", rowColor = "") => {
+      for (const td of tr.childNodes) {
+        td.style.backgroundColor = rowColor;
+      }
+      for (const tableRow of table.childNodes) {
+        const td = tableRow.childNodes[j];
+        td.style.backgroundColor = rowColor;
+      }
+      cell.style.backgroundColor = cellColor;
+    };
+
+    cell.addEventListener("mouseover", () => {
+      highlightCells("red", "green");
+    });
+
+    cell.addEventListener("mouseout", () => {
+      highlightCells();
+    });
+
+    tr.append(cell);
+  }
+
+  table.append(tr);
 }
 
-for (let j = 0; j < halfRow + 1; j++) {
-  const dots = "".padStart(dotsLength, ".");
-  const cage = "".padStart(cagesLength, "#");
-  console.log(dots + cage + dots);
-  cagesLength -= 2;
-  dotsLength++;
-}
+tableContainer.append(table);
