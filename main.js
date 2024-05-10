b68d498b713644e582ebfbc76dc06a75363b6ccf
Taras Samoilenko <tsamoilenko97@gmail.com>
2024-05-10 22:02:49 +0300

    lesson

--- main.js
+++ main.js
@@ -1,245 +1,184 @@
-class User {
-  constructor(name, lastName) {
-    this.name = name;
-    this.lastName = lastName;
-  }
+console.log(Redux);
+const counter = document.getElementById("counter");
 
-  age = 0;
+const actionType = "CHANGE_THEME";
+const increaseCounter = "INCREASE_COUNTER";
+const decreaseCounter = "DECREASE_COUNTER";
+const loadCart = "LOAD_CART";
+// const themeAction = {
+//   type: actionType,
+//   payload: "",
+// };
 
-  getAge() {
-    return this.age;
-  }
-
-  get fullName() {
-    return `${this.name} ${this.lastName}`;
-  }
-
-  set fullName(data) {
-    const [name, lastName] = data.split(" ");
-    this.name = name;
-    this.lastName = lastName;
-    return `${this.name} ${this.lastName}`;
-  }
-
-  getInfo = () => {
-    return this;
+const fetchCart = () => {
+  return (dispatch) => {
+    fetch("https://dummyjson.com/carts")
+      .then((res) => res.json())
+      .then((data) => dispatch({ type: loadCart, payload: data }));
   };
-}
-
-const user = new User("Taras", "S");
-console.log("🚀 ~ user==>>", user);
-
-function bar(func) {
-  return func();
-}
-
-// console.log("🚀 ~ bar(user.getAge)==>>", bar(user.getAge));
-
-console.log("🚀 ~ bar(user.getInfo)==>>", bar(user.getInfo));
-
-function UserFunc(name) {
-  this.age = 0;
-  this.name = name;
-  this.get = function () {
-    return this.age + 1;
-  };
-  this.getInfo = () => {
-    return this;
-  };
-}
-
-UserFunc.prototype.getAge = function () {
-  return this.age;
 };
 
-const userFunc = new UserFunc("Taras");
-
-class Pet {
-  constructor(petName, ownerName) {
-    this.petName = petName;
-    this.ownerName = ownerName;
+const themeActionCreator = (payload) => ({ type: actionType, payload });
+const themeReducer = (state = "light", action) => {
+  switch (action.type) {
+    case actionType:
+      return action.payload;
+    default:
+      return state;
   }
+};
 
-  getPetName = () => {
-    return this.petName;
+const counterReducer = (state = 0, action) => {
+  switch (action.type) {
+    case increaseCounter:
+      return state + 1;
+    case decreaseCounter:
+      return state - 1;
+    default:
+      return state;
+  }
+};
+
+const cartReducer = (state = {}, action) => {
+  switch (action.type) {
+    case loadCart:
+      return action.payload;
+    default:
+      return state;
+  }
+};
+
+const createStore = (rootReducer) => {
+  let state = {};
+  const listeners = [];
+
+  const dispatch = (action) => {
+    state = rootReducer(state, action);
+    listeners.forEach((callback) => {
+      callback();
+    });
+  };
+  const getState = () => {
+    return state;
+  };
+  const subscribe = (listener) => {
+    listeners.push(listener);
   };
 
-  getOwnerName = () => {
-    return this.ownerName;
+  return {
+    dispatch,
+    getState,
+    subscribe,
   };
+};
 
-  get petInfo() {
-    return `pet - ${this.petName}, owner - ${this.ownerName}`;
-  }
-
-  set petInfo(value) {
-    const [petName, ownerName = ""] = value.split(" ");
-    this.petName = petName;
-    this.ownerName = ownerName;
-    return `pet - ${this.petName}, owner - ${this.ownerName}`;
-  }
-}
-
-const pet = new Pet("Kit", "Taras");
-console.log("🚀 ~ pet==>>", pet);
-
-class Dog {
-  static animal = "dog";
-  static getAnimal = () => {
-    const age = this.age;
-    const animal = Dog.animal;
-    debugger;
-    return this;
+const combineReducers = (reducers) => {
+  return (state, action) => {
+    return Object.keys(reducers).reduce((nextState, key) => {
+      nextState[key] = reducers[key](state[key], action);
+      return nextState;
+    }, {});
   };
-  age = 0;
-  getAge = () => {
-    return this.age;
-  };
-  getInfo() {
-    const animal = Dog.animal;
-    const thisAnimal = this.animal;
-    debugger;
+};
+
+// custom
+// const rootReducer = combineReducers({
+//   theme: themeReducer,
+//   counter: counterReducer,
+// });
+// const store = createStore(rootReducer);
+
+// Redux
+const rootReducer = Redux.combineReducers({
+  theme: themeReducer,
+  counter: counterReducer,
+  cart: cartReducer,
+});
+const compose = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || Redux.compose;
+console.log("🚀 ~ Redux==>>", Redux);
+const store = Redux.createStore(
+  rootReducer,
+  {},
+  compose(Redux.applyMiddleware(ReduxThunk))
+);
+
+const updateClass = (className) => {
+  const btns = document.querySelectorAll("button");
+  for (let btn of btns) {
+    btn.classList.remove("light", "dark");
+    btn.classList.add(className);
   }
-}
+};
 
-const dogObj = new Dog();
-dogObj.age;
+store.subscribe(() => {
+  const state = store.getState();
+  counter.innerText = state.counter;
+  updateClass(state.theme);
+});
 
-1 + 1;
-"a" + "b";
+document.getElementById("theme").addEventListener("click", (event) => {
+  const lightThemeEnabled = event.target.classList.contains("light");
+  const action = themeActionCreator(lightThemeEnabled ? "dark" : "light");
+  store.dispatch(action);
+});
 
-class Shape {
-  constructor(color) {
-    this._color = color;
-  }
-  static value = "";
-  parentValue = "";
-  #secretColor = "blue";
-  #setSecretColor(newColor) {
-    this.#secretColor = newColor;
-  }
-  getColor = () => {
-    return this._color;
-  };
-  setColor = (newColor) => {
-    this._color = newColor;
-    this.#setSecretColor(newColor);
-  };
-  getArea() {
-    return "Abstract shape can't have area";
-  }
-}
+document.getElementById("increment").addEventListener("click", (event) => {
+  store.dispatch({ type: increaseCounter });
+});
+document.getElementById("decrement").addEventListener("click", (event) => {
+  store.dispatch({ type: decreaseCounter });
+});
+document.getElementById("load").addEventListener("click", (event) => {
+  store.dispatch(fetchCart());
+});
 
-class Rectangular extends Shape {
-  constructor(color, a, b) {
-    super(color);
-    this.a = a;
-    this.b = b;
-  }
-
-  getArea = () => {
-    return this.a * this.b;
-  };
-}
-
-class Circle extends Shape {
-  constructor(color, radius) {
-    super(color);
-    this.radius = radius;
-  }
-  getArea() {
-    const value = super.getArea();
-
-    return Math.PI * Math.pow(this.radius, 2);
-  }
-}
-
-const shape = new Shape("black");
-const rectangular = new Rectangular("black", 4, 8);
-console.log("🚀 ~ rectangular==>>", rectangular);
-
-const circle = new Circle("red", 4);
-
-class BankAccount {
-  constructor(balance, accountHolder) {
-    this.#balance = balance;
-    this.#accountHolder = accountHolder;
-  }
-  #balance = 0;
-  #accountHolder = "";
-
-  getBalance() {
-    return this.#balance;
+const printNumber = (to) => {
+  if (to === 1) {
+    console.log(to);
+    return;
   }
 
-  setBalance(value) {
-    if (typeof value === "number") {
-      this.#balance = value;
+  console.log(to);
+  printNumber(to - 1);
+};
+
+printNumber(5);
+
+const factorialLoop = (num) => {
+  let fact = 1;
+  for (let i = 1; i <= num; i++) {
+    fact *= i;
+  }
+  return fact;
+};
+
+const factorialRecursion = (num) => {
+  if (num === 1) {
+    return num;
+  }
+  const result = factorialRecursion(num - 1);
+  return num * result;
+};
+
+const fibNumber = (num) => {
+  return num <= 1 ? num : fibNumber(num - 1) + fibNumber(num - 2);
+};
+
+const fibWithCache = () => {
+  const map = new Map();
+
+  return function fib(num) {
+    if (map.has(num)) {
+      return map.get(num);
     }
-    return this;
-  }
-
-  getAccountHolder() {
-    return this.#accountHolder;
-  }
-
-  setAccountHolder(value) {
-    if (typeof value === "string") {
-      this.#accountHolder = value;
+    if (num <= 1) {
+      return num;
     }
-    return this;
-  }
-}
+    const result = fib(num - 1) + fib(num - 2);
+    map.set(num, result);
+    return result;
+  };
+};
 
-const bankAccount = new BankAccount(100, "T");
-
-class Animal {
-  constructor(name) {
-    this.name = name;
-  }
-  getInfo() {
-    return `name: ${this.name}`;
-  }
-}
-class Mammal extends Animal {
-  constructor(name, live) {
-    super(name);
-    this.live = live;
-  }
-  run() {
-    return `${this.name} is running`;
-  }
-  getInfo() {
-    return `${super.getInfo()}, live: ${this.live}`;
-  }
-}
-class Bird extends Animal {
-  constructor(name, live) {
-    super(name);
-    this.live = live;
-  }
-  fly() {
-    return `${this.name} is flying`;
-  }
-  getInfo() {
-    return `${super.getInfo()}, live: ${this.live}`;
-  }
-}
-
-class Penguin extends Bird {
-  constructor(name, live, species) {
-    super(name, live);
-    this.species = species;
-  }
-  fly() {
-    return "penguins don't fly";
-  }
-  swim() {
-    return "penguin is swimming";
-  }
-  getInfo() {
-    return `${super.getInfo()}, species: ${this.species}`;
-  }
-}
-
-const penguin = new Penguin("Jack", "Antarctica", "Imperial");
+const fibCache = fibWithCache();
+fibCache(80);
