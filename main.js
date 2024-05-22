42d20d888cb01dd1e81fafcefb1355c8207977eb
Taras Samoilenko <tsamoilenko97@gmail.com>
2024-05-22 18:32:56 +0300

    lesson

--- main.js
+++ main.js
@@ -1,184 +1,74 @@
-console.log(Redux);
-const counter = document.getElementById("counter");
-
-const actionType = "CHANGE_THEME";
-const increaseCounter = "INCREASE_COUNTER";
-const decreaseCounter = "DECREASE_COUNTER";
-const loadCart = "LOAD_CART";
-// const themeAction = {
-//   type: actionType,
-//   payload: "",
-// };
-
-const fetchCart = () => {
-  return (dispatch) => {
-    fetch("https://dummyjson.com/carts")
-      .then((res) => res.json())
-      .then((data) => dispatch({ type: loadCart, payload: data }));
+class PostService {
+  allPost = null;
+  post = null;
+  searchPost = null;
+  newPost = null;
+  updatedPost = null;
+  deletePost = null;
+  getAllPosts = async () => {
+    const res = await fetch("https://dummyjson.com/posts");
+    const data = await res.json();
+    this.allPost = data;
+    return this;
   };
-};
-
-const themeActionCreator = (payload) => ({ type: actionType, payload });
-const themeReducer = (state = "light", action) => {
-  switch (action.type) {
-    case actionType:
-      return action.payload;
-    default:
-      return state;
-  }
-};
-
-const counterReducer = (state = 0, action) => {
-  switch (action.type) {
-    case increaseCounter:
-      return state + 1;
-    case decreaseCounter:
-      return state - 1;
-    default:
-      return state;
-  }
-};
-
-const cartReducer = (state = {}, action) => {
-  switch (action.type) {
-    case loadCart:
-      return action.payload;
-    default:
-      return state;
-  }
-};
-
-const createStore = (rootReducer) => {
-  let state = {};
-  const listeners = [];
-
-  const dispatch = (action) => {
-    state = rootReducer(state, action);
-    listeners.forEach((callback) => {
-      callback();
+  getPost = async (id) => {
+    const res = await fetch(`https://dummyjson.com/posts/${id}`);
+    const data = await res.json();
+    this.post = data;
+    return this;
+  };
+  searchPost = async (limit = 10, skip = 0) => {
+    const url = new URL("https://dummyjson.com/posts");
+    url.searchParams.set("limit", limit);
+    url.searchParams.set("skip", skip);
+    url.searchParams.set("select", "title,body");
+    const res = await fetch(url.toString());
+    const data = await res.json();
+    this.searchPost = data;
+    return this;
+  };
+  createPost = async (postData) => {
+    const res = await fetch("https://dummyjson.com/posts/add", {
+      method: "post",
+      headers: { "Content-Type": "application/json" },
+      body: JSON.stringify(postData),
     });
+    const data = await res.json();
+    this.newPost = data;
+    return this;
   };
-  const getState = () => {
-    return state;
+  updatePost = async (id, postData) => {
+    const res = await fetch(`https://dummyjson.com/posts/${id}`, {
+      method: "put",
+      headers: { "Content-Type": "application/json" },
+      body: JSON.stringify(postData),
+    });
+    const data = await res.json();
+    this.updatedPost = data;
+    return this;
   };
-  const subscribe = (listener) => {
-    listeners.push(listener);
+  deletePost = async (id) => {
+    const res = await fetch(`https://dummyjson.com/posts/${id}`, {
+      method: "delete",
+    });
+    const data = await res.json();
+    this.deletePost = data;
+    return this;
   };
+}
 
-  return {
-    dispatch,
-    getState,
-    subscribe,
-  };
+const render = (...posts) => {
+  posts.forEach((item) => {
+    const card = document.createElement("div");
+    const title = document.createElement("h1");
+    const text = document.createElement("p");
+
+    title.innerText = item.title;
+    text.innerText = item.body;
+
+    card.append(title, text);
+    document.body.append(card);
+  });
 };
 
-const combineReducers = (reducers) => {
-  return (state, action) => {
-    return Object.keys(reducers).reduce((nextState, key) => {
-      nextState[key] = reducers[key](state[key], action);
-      return nextState;
-    }, {});
-  };
-};
-
-// custom
-// const rootReducer = combineReducers({
-//   theme: themeReducer,
-//   counter: counterReducer,
-// });
-// const store = createStore(rootReducer);
-
-// Redux
-const rootReducer = Redux.combineReducers({
-  theme: themeReducer,
-  counter: counterReducer,
-  cart: cartReducer,
-});
-const compose = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || Redux.compose;
-console.log("🚀 ~ Redux==>>", Redux);
-const store = Redux.createStore(
-  rootReducer,
-  {},
-  compose(Redux.applyMiddleware(ReduxThunk))
-);
-
-const updateClass = (className) => {
-  const btns = document.querySelectorAll("button");
-  for (let btn of btns) {
-    btn.classList.remove("light", "dark");
-    btn.classList.add(className);
-  }
-};
-
-store.subscribe(() => {
-  const state = store.getState();
-  counter.innerText = state.counter;
-  updateClass(state.theme);
-});
-
-document.getElementById("theme").addEventListener("click", (event) => {
-  const lightThemeEnabled = event.target.classList.contains("light");
-  const action = themeActionCreator(lightThemeEnabled ? "dark" : "light");
-  store.dispatch(action);
-});
-
-document.getElementById("increment").addEventListener("click", (event) => {
-  store.dispatch({ type: increaseCounter });
-});
-document.getElementById("decrement").addEventListener("click", (event) => {
-  store.dispatch({ type: decreaseCounter });
-});
-document.getElementById("load").addEventListener("click", (event) => {
-  store.dispatch(fetchCart());
-});
-
-const printNumber = (to) => {
-  if (to === 1) {
-    console.log(to);
-    return;
-  }
-
-  console.log(to);
-  printNumber(to - 1);
-};
-
-printNumber(5);
-
-const factorialLoop = (num) => {
-  let fact = 1;
-  for (let i = 1; i <= num; i++) {
-    fact *= i;
-  }
-  return fact;
-};
-
-const factorialRecursion = (num) => {
-  if (num === 1) {
-    return num;
-  }
-  const result = factorialRecursion(num - 1);
-  return num * result;
-};
-
-const fibNumber = (num) => {
-  return num <= 1 ? num : fibNumber(num - 1) + fibNumber(num - 2);
-};
-
-const fibWithCache = () => {
-  const map = new Map();
-
-  return function fib(num) {
-    if (map.has(num)) {
-      return map.get(num);
-    }
-    if (num <= 1) {
-      return num;
-    }
-    const result = fib(num - 1) + fib(num - 2);
-    map.set(num, result);
-    return result;
-  };
-};
-
-const fibCache = fibWithCache();
-fibCache(80);
+const postService = new PostService();
